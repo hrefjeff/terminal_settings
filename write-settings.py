f1c65d2fc15e10a4c32fe0558c5f53ba07e5d955
Jeffrey Allen <2584585+hrefjeff@users.noreply.github.com>
2021-04-29 15:17:43 -0500

    Create write-settings.py

---
+++ write-settings.py
@@ -0,0 +1,26 @@
+import os
+
+# Set up files
+
+home_dir = os.path.expanduser('~')
+#filename = 'vimrc'
+#filename = 'bashrc'
+filename = 'aliases'
+
+thefile = os.path.join(home_dir, '.' + filename)
+
+try:
+    # a = open for writing, exists = appends, doesn't exist = creates it
+    # https://stackoverflow.com/a/1466036
+    with open(thefile, 'a') as writefile, open(filename,'r') as readfile:
+        
+        # read content from readfile
+        for line in readfile:
+
+            # Write settings to bashrc
+            writefile.write(line)
+
+        print('Done!')
+
+except Exception as e:
+    print(e)
