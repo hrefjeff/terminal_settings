8d213ade96ebb4cdd59a664526fe31721ba1834e
Jeffrey Allen <2584585+hrefjeff@users.noreply.github.com>
2022-03-14 13:16:51 -0500

    Update write-settings.py

--- write-settings.py
+++ write-settings.py
@@ -1,27 +1,26 @@
 import os
 
-# Set up files
+def main(): 
+    # Uncomment which settings file to set up
+    home_dir = os.path.expanduser('~')
+    filename = 'vimrc'
+    #filename = 'bashrc'
+    #filename = 'gitconfig'
+    #filename = 'aliases'
 
-home_dir = os.path.expanduser('~')
-#filename = 'vimrc'
-#filename = 'bashrc'
-#filename = 'gitconfig'
-filename = 'aliases'
+    thefile = os.path.join(home_dir, '.' + filename)
 
-thefile = os.path.join(home_dir, '.' + filename)
-
-try:
-    # a = open for writing, exists = appends, doesn't exist = creates it
-    # https://stackoverflow.com/a/1466036
-    with open(thefile, 'a') as writefile, open(filename,'r') as readfile:
-        
-        # read content from readfile
-        for line in readfile:
-
-            # Write settings to bashrc
-            writefile.write(line)
+    try:
+        # a = open for writing, exists = appends, doesn't exist = creates it
+        # https://stackoverflow.com/a/1466036
+        with open(thefile, 'a') as writefile, open(filename,'r') as readfile:
+            for line in readfile: # read from repo settings file
+                writefile.write(line) # write to local settings file
 
         print('Done!')
 
-except Exception as e:
-    print(e)
+    except Exception as e:
+        print(e)
+
+if __name__ == '__main__':
+    main()
